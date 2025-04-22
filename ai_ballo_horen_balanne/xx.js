8684e73cf9768261a920b8760239d90bffadae72
SOUL KILLER OFC <153274881+Ayeshherath@users.noreply.github.com>
2025-04-22 16:29:08 +0530

    Update xx.js

--- ai_ballo_horen_balanne/xx.js
+++ ai_ballo_horen_balanne/xx.js
@@ -1,6 +1,6 @@
 // XVIDEO DOWNLOAD COMMAND
 
-const { cmd } = require('../lib/command')
+const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
 
 const apilink = 'https://www.dark-yasiya-api.site/' // API LINK ( DO NOT CHANGE THIS!! )
