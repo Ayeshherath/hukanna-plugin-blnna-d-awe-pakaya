f3f0c5c3874d3d67d1823eb977a47a026aa9e72a
SOUL KILLER OFC <153274881+Ayeshherath@users.noreply.github.com>
2025-04-22 15:39:30 +0530

    Update alivenew.js

--- ai_ballo_horen_balanne/alivenew.js
+++ ai_ballo_horen_balanne/alivenew.js
@@ -1,7 +1,7 @@
 //========= alive command ===========
 
-const config = require('../settings')
-const {cmd , commands} = require('../lib/command')
+const config = require('../config')
+const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
